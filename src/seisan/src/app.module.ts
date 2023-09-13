7bae6a92d24a06f4a86b342ceee30778f1f1a2bf
yukimasaki <woodenhoe@gmail.com>
2023-09-13 07:46:14 +0000

    feat: npx nest g resource member

--- src/seisan/src/app.module.ts
+++ src/seisan/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './common/prisma/prisma.service';
 import { GroupModule } from './group/group.module';
+import { MemberModule } from './member/member.module';
 
 @Module({
-  imports: [UserModule, GroupModule],
+  imports: [UserModule, GroupModule, MemberModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
