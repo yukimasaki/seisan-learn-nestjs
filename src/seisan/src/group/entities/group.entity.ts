cd3f4ac39b1ec3fd0e1e3c9e9c98800c17a2ce55
yukimasaki <woodenhoe@gmail.com>
2023-09-13 07:34:25 +0000

    feat: group.entity作成

--- src/seisan/src/group/entities/group.entity.ts
+++ src/seisan/src/group/entities/group.entity.ts
@@ -1,1 +1,17 @@
-export class Group {}
+import { IsInt, IsPositive, IsString, Length, MaxLength } from "class-validator";
+
+export class Group {
+  @IsInt()
+  @IsPositive()
+  id!: number;
+
+  @IsString()
+  @Length(32)
+  uid!: string;
+
+  @IsString()
+  @MaxLength(255)
+  displayName!: string;
+}
+
+export class GroupResponse extends Group {}
