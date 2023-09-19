51d3005ce15ae4552b639ec53bf71a6168030000
yukimasaki <woodenhoe@gmail.com>
2023-09-19 08:12:43 +0000

    feat: transaction: createWithTransaction実装(未テスト)

--- src/seisan/src/transaction/transaction.service.ts
+++ src/seisan/src/transaction/transaction.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { CreateTransactionComplex, CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { PrismaService } from 'src/common/prisma/prisma.service';
+import { CreatePaymentDto, CreatePaymentOmitTransactionId } from 'src/payment/dto/create-payment.dto';
+import { CreateBalanceDto, CreateBalanceOmitTransactionId } from 'src/balance/dto/create-balance.dto';
+import { TransactionResponse } from './entities/transaction.entity';
 
 @Injectable()
 export class TransactionService {
@@ -9,9 +12,50 @@ export class TransactionService {
     private readonly prisma: PrismaService,
   ){}
 
-  async create(createTransactionDto: CreateTransactionDto) {
-    return await this.prisma.transaction.create({
-      data: createTransactionDto
+  async createWithTransaction(
+    createTransactionComplex: CreateTransactionComplex,
+  ) {
+    // 複合化されたリクエストボディを個別のオブジェクトに分割する
+    const createTransactionDto = createTransactionComplex.createTransactionDto;
+    const createPaymentOmitTransactionId = createTransactionComplex.createPaymentOmitTransactionId;
+    const createBalanceOmitTransactionId = createTransactionComplex.createBalanceOmitTransactionId;
+
+    // 1. Prismaのトランザクション処理を開始
+    return await this.prisma.$transaction(async (prisma) => {
+      // 2. Transactionを作成
+      const transaction: TransactionResponse = await this.prisma.transaction.create({
+        data: createTransactionDto
+      });
+
+      // 3. transactionIdを取得
+      const transactionId = transaction.id;
+      // transactionIdを付与する (Payment)
+      const createPaymentDto: CreatePaymentDto[] = createPaymentOmitTransactionId.map(omitTransactionId => ({
+        ...omitTransactionId,
+        transactionId,
+      }));
+      // transactionIdを付与する (Balance)
+      const createBalanceDto: CreateBalanceDto[] = createBalanceOmitTransactionId.map(omitTransactionId => ({
+        ...omitTransactionId,
+        transactionId,
+      }));
+
+      // APIへのアクセスをPromise.allで並列処理し高速化する
+      Promise.all([
+        // 4. Paymentを作成
+        await this.prisma.payment.createMany({
+          data: createPaymentDto
+        }),
+        // 5. Balanceを作成
+        await this.prisma.balance.createMany({
+          data: createBalanceDto
+        }),
+      ])
+      .then(() => console.log(`トランザクション処理に成功しました`))
+      .catch(err => {
+        console.log(`トランザクション処理に失敗しました`)
+        console.log(err)
+      });
     });
   }
 
