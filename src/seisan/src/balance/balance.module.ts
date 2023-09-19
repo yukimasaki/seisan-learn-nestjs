06b15f278195b5392878db1de75292d7e39670c6
yukimasaki <woodenhoe@gmail.com>
2023-09-19 02:20:40 +0000

    feat: BalanceModuleにPrismaServiceを追記

--- src/seisan/src/balance/balance.module.ts
+++ src/seisan/src/balance/balance.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BalanceService } from './balance.service';
 import { BalanceController } from './balance.controller';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Module({
   controllers: [BalanceController],
-  providers: [BalanceService],
+  providers: [
+    BalanceService,
+    PrismaService,
+  ],
 })
 export class BalanceModule {}
