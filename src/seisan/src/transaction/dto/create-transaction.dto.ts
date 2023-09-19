6c620506f8f25d8b85ebacf985b9f098e9dc4e59
yukimasaki <woodenhoe@gmail.com>
2023-09-19 08:12:05 +0000

    feat: リクエストボディを複合化

--- src/seisan/src/transaction/dto/create-transaction.dto.ts
+++ src/seisan/src/transaction/dto/create-transaction.dto.ts
@@ -1,3 +1,10 @@
 import { Transaction } from "../entities/transaction.entity";
+import { CreatePaymentOmitTransactionId } from "src/payment/dto/create-payment.dto";
+import { CreateBalanceOmitTransactionId } from "src/balance/dto/create-balance.dto";
 
 export class CreateTransactionDto extends Transaction {}
+export class CreateTransactionComplex {
+  createTransactionDto: CreateTransactionDto;
+  createPaymentOmitTransactionId: CreatePaymentOmitTransactionId[];
+  createBalanceOmitTransactionId: CreateBalanceOmitTransactionId[];
+}
