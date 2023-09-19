32ef26c4192116d77ec424b4ec60ec58bf1bd376
yukimasaki <woodenhoe@gmail.com>
2023-09-19 07:40:37 +0000

    feat: スペル修正

--- src/seisan/src/balance/dto/create-balance.dto.ts
+++ src/seisan/src/balance/dto/create-balance.dto.ts
@@ -2,4 +2,4 @@ import { OmitType } from "@nestjs/mapped-types";
 import { Balance } from "../entities/balance.entity";
 
 export class CreateBalanceDto extends Balance {}
-export class CreatePaymentOmitTransactionId extends OmitType(Balance, ['transactionId']) {}
+export class CreateBalanceOmitTransactionId extends OmitType(Balance, ['transactionId']) {}
