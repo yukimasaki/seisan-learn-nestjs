b246709913c2874715d0fb8e34cf74b963689a8f
yukimasaki <woodenhoe@gmail.com>
2023-09-19 07:39:19 +0000

    feat: PaymentとBalanceのOmitTypeを作成

--- src/seisan/src/balance/dto/create-balance.dto.ts
+++ src/seisan/src/balance/dto/create-balance.dto.ts
@@ -1,3 +1,5 @@
+import { OmitType } from "@nestjs/mapped-types";
 import { Balance } from "../entities/balance.entity";
 
 export class CreateBalanceDto extends Balance {}
+export class CreatePaymentOmitTransactionId extends OmitType(Balance, ['transactionId']) {}
--- src/seisan/src/payment/dto/create-payment.dto.ts
+++ src/seisan/src/payment/dto/create-payment.dto.ts
@@ -1,3 +1,5 @@
+import { OmitType } from "@nestjs/mapped-types";
 import { Payment } from "../entities/payment.entity";
 
 export class CreatePaymentDto extends Payment {}
+export class CreatePaymentOmitTransactionId extends OmitType(Payment, ['transactionId']) {}
