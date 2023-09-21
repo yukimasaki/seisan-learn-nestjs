1e14c808b19b93a3afe88cbefa40f23c728d1f1d
yukimasaki <woodenhoe@gmail.com>
2023-09-21 06:20:16 +0000

    CreateTransactionDtoをOmitTypeでid, editorIdを除外

--- src/seisan/src/transaction/dto/create-transaction.dto.ts
+++ src/seisan/src/transaction/dto/create-transaction.dto.ts
@@ -1,8 +1,9 @@
 import { Transaction } from "../entities/transaction.entity";
 import { CreatePaymentOmitTransactionId } from "src/payment/dto/create-payment.dto";
 import { CreateBalanceOmitTransactionId } from "src/balance/dto/create-balance.dto";
+import { OmitType } from "@nestjs/mapped-types";
 
-export class CreateTransactionDto extends Transaction {}
+export class CreateTransactionDto extends OmitType(Transaction, ['id', 'editorId']) {}
 export class CreateTransactionComplex {
   createTransactionDto: CreateTransactionDto;
   createPaymentOmitTransactionId: CreatePaymentOmitTransactionId[];
