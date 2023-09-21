7614df5c53a653bcee879e98986e92afb4a8c70a
yukimasaki <woodenhoe@gmail.com>
2023-09-21 07:58:50 +0000

    feat: OmitTypeでidを除外

--- src/seisan/src/balance/dto/create-balance.dto.ts
+++ src/seisan/src/balance/dto/create-balance.dto.ts
@@ -1,5 +1,9 @@
 import { OmitType } from "@nestjs/mapped-types";
 import { Balance } from "../entities/balance.entity";
 
-export class CreateBalanceDto extends Balance {}
-export class CreateBalanceOmitTransactionId extends OmitType(Balance, ['transactionId']) {}
+export class CreateBalanceDto extends OmitType(
+  Balance, ['id']
+) {}
+export class CreateBalanceOmitTransactionId extends OmitType(
+  Balance, ['id', 'transactionId']
+) {}
--- src/seisan/src/payment/dto/create-payment.dto.ts
+++ src/seisan/src/payment/dto/create-payment.dto.ts
@@ -1,5 +1,9 @@
 import { OmitType } from "@nestjs/mapped-types";
 import { Payment } from "../entities/payment.entity";
 
-export class CreatePaymentDto extends Payment {}
-export class CreatePaymentOmitTransactionId extends OmitType(Payment, ['transactionId']) {}
+export class CreatePaymentDto extends OmitType(
+  Payment, ['id']
+) {}
+export class CreatePaymentOmitTransactionId extends OmitType(
+  Payment, ['id', 'transactionId']
+) {}
