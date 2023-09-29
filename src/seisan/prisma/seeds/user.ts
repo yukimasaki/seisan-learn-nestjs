54586ae4c356bff17e9af1227c769d3d413a69d4
yukimasaki <woodenhoe@gmail.com>
2023-09-29 02:13:56 +0000

    feat: seed時のパスワードのハッシュ化処理を追加

--- src/seisan/prisma/seeds/user.ts
+++ src/seisan/prisma/seeds/user.ts
@@ -1,4 +1,8 @@
+import { CreateUserDto } from '@@nest/user/dto/create-user.dto';
+import { User } from '@@nest/user/entities/user.entity';
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
 const prisma = new PrismaClient();
 
 export const deleteUser = async () => {
@@ -13,14 +17,16 @@ export const createUser = async () => {
 
   const userCount = 4;
 
+  const saltOrRounds = 10;
+  const rawPassword = 'password';
+  const hash = await bcrypt.hash(rawPassword, saltOrRounds);
+
   await prisma.user.createMany({
-    data: Array(userCount)
-    .fill(0)
-    .map((_, index) => ({
+    data: Array.from({ length: userCount }, (_, index)=> ({
       email: `user${index + 1}@example.com`,
       displayName: `User ${index + 1}`,
       membership: membership[Math.floor(Math.random() * membership.length)],
-      hashedPassword: ``,
+      hashedPassword: hash,
     })),
   });
 }
