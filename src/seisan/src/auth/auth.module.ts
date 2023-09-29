9b13b20551c4c7f6961601921414426089910340
yukimasaki <woodenhoe@gmail.com>
2023-09-29 02:36:27 +0000

    feat: auth作成中

--- src/seisan/src/auth/auth.module.ts
+++ src/seisan/src/auth/auth.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PrismaService } from '@@nest/common/prisma/prisma.service';
+import { UserService } from '@@nest/user/user.service';
 
 @Module({
-  providers: [AuthService]
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    UserService,
+    PrismaService,
+  ],
 })
 export class AuthModule {}
--- src/seisan/src/auth/auth.service.ts
+++ src/seisan/src/auth/auth.service.ts
@@ -1,4 +1,26 @@
+import { UserOmitPassword, UserResponse } from '@@nest/user/entities/user.entity';
+import { UserService } from '@@nest/user/user.service';
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+  ) {}
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<UserOmitPassword | null> {
+    const user: UserResponse = await this.userService.findOne(email);
+    const isMatch: boolean = await bcrypt.compare(password, user.hashedPassword);
+
+    if (user && isMatch) {
+      const { hashedPassword, ...userOmitPassword } = user;
+      return userOmitPassword;
+    }
+
+    return null;
+  }
+}
