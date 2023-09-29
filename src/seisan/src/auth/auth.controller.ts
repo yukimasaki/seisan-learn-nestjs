6d100e2ceefd066a8ddf4ce33cc428e89d8fea31
yukimasaki <woodenhoe@gmail.com>
2023-09-29 06:42:56 +0000

    feat: JwtAuthGuard実装

--- src/seisan/src/auth/auth.controller.ts
+++ src/seisan/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,11 @@ export class AuthController {
   @Post('login')
   async login(@Request() request) {
     return this.authService.login(request.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() request) {
+    return request.user;
   }
 }
