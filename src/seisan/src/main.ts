749cecf9cb59147cde48c4105549c489619b7c75
yukimasaki <woodenhoe@gmail.com>
2023-11-14 07:20:45 +0000

    chore: APIテストツールDredd用のyamlファイルの出力設定をmain.tsへ追記

--- src/seisan/src/main.ts
+++ src/seisan/src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as path from 'path';
+import { writeFileSync } from 'fs';
+import { dump } from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -22,6 +25,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
+  const outputPath = path.resolve(`${process.cwd()}/test/dredd/`, 'openapi.yml');
+  writeFileSync(outputPath, dump(document, {}));
 
   await app.listen(3000);
 }
