9e60ea9d61d9be42487efba207ee3e0b88794428
yukimasaki <woodenhoe@gmail.com>
2023-11-14 07:01:55 +0000

    docs: GroupEntity

--- src/seisan/src/group/entities/group.entity.ts
+++ src/seisan/src/group/entities/group.entity.ts
@@ -1,17 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, IsPositive, IsString, Length, MaxLength } from "class-validator";
 
 export class Group {
+  @ApiProperty({
+    example: '1',
+    description: 'グループID',
+  })
   @IsInt()
   @IsPositive()
   id: number;
 
+  @ApiProperty({
+    example: 'c68e0836b2e04476a18eeb17a5c3385b',
+    description: 'Webアプリケーション用の固有ID。UUIDv4からハイフンを除いた文字列を使用。',
+  })
   @IsString()
   @Length(32)
   uid: string;
 
+  @ApiProperty({
+    example: 'グループA',
+    description: 'グループの表示名',
+  })
   @IsString()
   @MaxLength(255)
   displayName: string;
 }
 
-export class GroupResponse extends Group {}
+export class GroupResponse extends Group { }
