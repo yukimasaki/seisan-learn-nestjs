1d552a9a9bdd9c4422daba80c675d5a0ec0347fd
yukimasaki <woodenhoe@gmail.com>
2023-11-15 06:47:15 +0000

    docs: GET /balances/:id

--- src/seisan/src/balance/balance.controller.ts
+++ src/seisan/src/balance/balance.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Body, Patch, Param } from '@nestjs/common';
 import { BalanceService } from './balance.service';
 import { UpdateBalanceDto } from './dto/update-balance.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiProduces, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SummarizeApiResponse } from '@@nest/common/decorators/summarize-api-response.decorator';
+import { Balance } from './entities/balance.entity';
 
 @Controller('balances')
 @ApiTags('/balances')
@@ -11,6 +12,13 @@ export class BalanceController {
   constructor(private readonly balanceService: BalanceService) { }
 
   @Get()
+  @ApiProduces('application/json; charset=utf-8')
+  @ApiOperation({ summary: '単体取得API' })
+  @ApiResponse({
+    status: 201,
+    description: '指定したIDの貸借情報を返却',
+    type: Balance,
+  })
   findAll() {
     return this.balanceService.findAll();
   }
