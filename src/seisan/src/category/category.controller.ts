5a86060ec32099493b83b8555859d21a75f264e9
yukimasaki <woodenhoe@gmail.com>
2023-11-15 05:10:24 +0000

    docs: POST /categories

--- src/seisan/src/category/category.controller.ts
+++ src/seisan/src/category/category.controller.ts
@@ -2,8 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Query, BadRequestExc
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiProduces, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SummarizeApiResponse } from '@@nest/common/decorators/summarize-api-response.decorator';
+import { Category } from './entities/category.entity';
 
 @Controller('categories')
 @ApiTags('/categories')
@@ -12,6 +13,13 @@ export class CategoryController {
   constructor(private readonly categoryService: CategoryService) { }
 
   @Post()
+  @ApiProduces('application/json; charset=utf-8')
+  @ApiOperation({ summary: '単体登録API' })
+  @ApiResponse({
+    status: 201,
+    description: '登録後のカテゴリー情報を返却',
+    type: Category,
+  })
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
