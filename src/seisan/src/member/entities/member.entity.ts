e9e346157e0ed106d5aeb5a8828fa8f8ba588164
yukimasaki <woodenhoe@gmail.com>
2023-11-15 04:29:16 +0000

    docs: MemberEntity

--- src/seisan/src/member/entities/member.entity.ts
+++ src/seisan/src/member/entities/member.entity.ts
@@ -1,13 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, IsPositive } from "class-validator";
 
 export class Member {
+  @ApiProperty({
+    example: '1',
+    description: 'メンバーID',
+  })
   @IsInt()
   @IsPositive()
   userId: number;
 
+  @ApiProperty({
+    example: 'c68e0836b2e04476a18eeb17a5c3385b',
+    description: 'グループID',
+  })
   @IsInt()
   @IsPositive()
   groupId: number;
 }
 
-export class MemberResponse extends Member {}
+export class MemberResponse extends Member { }
